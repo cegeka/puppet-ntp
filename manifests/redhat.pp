5e206bdf3a8f659e29f47f5d91be44fcef0fa158
fdammeke <fabian.dammekens@cegeka.be>
2014-02-13 15:43:53 +0100

    Security measures + default ntp servers if ntp_servers is undef

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -13,6 +13,7 @@ class ntp::redhat {
     ensure  => file,
     content => template("${module_name}/ntp.conf.erb"),
     require => Package['ntp'],
+    notify  => Service['ntpd']
   }
 
   service { 'ntpd' :
