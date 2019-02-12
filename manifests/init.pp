81c86daa1e52e35bf1bb212ba6a2dffcec10e42c
Vandevoort Tim <tim.vandevoort@cegeka.com>
2019-02-12 16:37:30 +0100

    [CGK] Config needed to run Oracle Linux 7 in Vagrant

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,7 +18,7 @@ class ntp($ntp_servers=undef) {
     $real_ntp_servers = $ntp_servers
   }
   case $::operatingsystem {
-    'RedHat', 'CentOS' : { include ntp::redhat }
+    'RedHat', 'CentOS', 'OracleLinux' : { include ntp::redhat }
     'Debian', 'Ubuntu' : { include ntp::debian }
     default : { fail("Operatingsystem ${::operatingsystem} not supported")}
   }
